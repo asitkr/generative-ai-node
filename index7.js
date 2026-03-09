98b9936f81a43e7ef226a29e189645e3c4467608
asitkr <krsinhasaurabh@gmail.com>
2026-03-09 20:47:18 +0530

    basic config with gemini

---
+++ index7.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from "express";
+import { writeFileSync } from 'fs';
+import { GoogleGenAI } from "@google/genai";
+
+// Gemini setup with key in Node js
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+const genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_KEY });
+
+async function main() {
+    const response = await genAI.models.generateContent({
+        model: "gemini-3-flash-preview",
+        contents: "table of 20 and 30",
+    });
+
+    console.log(response.text);
+}
+
+main();
