fce3a34634859207924a4aa71382a3bfb28d2ba7
asitkr <krsinhasaurabh@gmail.com>
2026-03-09 22:31:09 +0530

    Read & Understand Image using Google Gen AI

--- index.js
+++ index.js
@@ -1,68 +1,36 @@
 import 'dotenv/config';
-import path from 'path';
+import { readFileSync } from 'fs';
 import express from "express";
 import { GoogleGenAI } from "@google/genai";
-
-// Gemini Basic Config Params Explained
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 const genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_KEY });
 
-// app.get("/", async (req, res) => {
-//         const response = await genAI.models.generateContentStream({
-//             // model: "gemini-3-flash-preview",
-//             model: "gemini-2.5-flash",
-//             contents: "Tell me about AI in detail",
-//         });
+async function main() {
+    const base64Img = readFileSync("lavender.png", { encoding: "base64" });
 
-//         for await (const chunk of response) {
-//             const text = chunk.text;
-
-//             // console.log(text);
-//             if(text) {
-//                 res.write(text);
-//             }
-//         }
-
-//         res.end("--------------------------------- Content Generation Completed ---------------------------------");
-// });
-
-// app.listen(3400);
-
-
-app.get("/", (req, res) => {
-    res.sendFile(path.resolve("public/index.html"));
-});
-
-function cleanText(text) {
-  return text
-    .replace(/#{1,6}\s?/g, "")                 // remove headings ##
-    .replace(/\*\*/g, "")                      // remove **
-    .replace(/^\s*[\*\-\•]\s+/gm, "");         // remove bullets *, -, •
-}
-
-// stream AI response
-app.post("/", async (req, res) => {
-
-    res.setHeader("Content-Type", "text/plain; charset=utf-8");
-    res.setHeader("Transfer-Encoding", "chunked");
-
-    const response = await genAI.models.generateContentStream({
+    const response = await genAI.models.generateContent({
         model: "gemini-2.5-flash",
-        contents: "Tell me about AI in detail",
+        contents: [
+            {
+                role: "user",
+                parts: [
+                    { text: "read text from this image" },
+                    { text: "tell me the color combination" },
+                    {
+                        inlineData: {
+                            mimeType: "image/png",
+                            data: base64Img,
+                        }
+                    }
+                ]
+            }
+        ],
     });
 
-    for await (const chunk of response) {
-        if (chunk.text) {
-            res.write(cleanText(chunk.text));
-        }
-    }
+    console.log(response.text);
+}
 
-    res.end("\n\n----- Completed -----");
-});
-
-app.listen(3400, () => {
-    console.log("Server running at http://localhost:3400");
-});
+main();
